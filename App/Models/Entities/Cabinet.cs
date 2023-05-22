e0f5d5d0c15d72ccf9edcbde8c910a0afa1d566e
Izuard <sasha.20133@gmail.com>
2023-05-22 18:36:04 +0300

    Add Cabinet entity

--- App/Models/Entities/Cabinet.cs
+++ App/Models/Entities/Cabinet.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace MedApp.Models.Entities
 {
     public class Cabinet
     {
+        /// <summary>
+        /// Id of the cabinet
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Id of the department
+        /// </summary>
+        public int DepartmentId { get; set; }
+
+        /// <summary>
+        /// Number of the cabinet, example: A-207
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Name of the cabinet 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// EF navigation property
+        /// </summary>
+        public Department Department { get; set; }
+
+        /// <summary>
+        /// EF navigation property
+        /// </summary>
+        public ICollection<Examination> Examinations { get; set; }
+
+        /// <summary>
+        /// EF navigation property
+        /// </summary>
+        public ICollection<Transaction> OldCabinets { get; set; }
+
+        /// <summary>
+        /// EF navigation property
+        /// </summary>
+        public ICollection<Transaction> NewCabinets { get; set; }
     }
 }
