3db336939369ec8690f6760e72c5df1d9ba1731b
Izuard <sasha.20133@gmail.com>
2023-05-22 18:55:22 +0300

    Add PatientInChamber entity

--- App/Models/Entities/PatientInChamber.cs
+++ App/Models/Entities/PatientInChamber.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MedApp.Models.Entities
 {
     public class PatientInChamber
     {
+        /// <summary>
+        /// Id of the medical card (patient)
+        /// </summary>
+        public int MedCardId { get; set; }
+
+        /// <summary>
+        /// Id of the chamber
+        /// </summary>
+        public int ChamberId { get; set; }
+
+        /// <summary>
+        /// EF navigation property
+        /// </summary>
+        public MedCard MedCard { get; set; }
+
+        /// <summary>
+        /// EF navigation property
+        /// </summary>
+        public Chamber Chamber { get; set; }
     }
 }
