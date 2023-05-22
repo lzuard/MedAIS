aefdaaa4e1ca47bec7ba6a9171f1c298e815b131
Izuard <sasha.20133@gmail.com>
2023-05-22 18:05:32 +0300

    Add MKB entity

---
+++ App/Models/Entities/Mkb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace MedApp.Models.Entities
+{
+    public class Mkb
+    {
+        /// <summary>
+        /// Id of the MKB-10 code
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Actual MKB-10 code
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Name of the diseases
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// EF navigation property
+        /// </summary>
+        public ICollection<Diagnosis> DiagnosisCollection { get; set; }
+
+    }
+}
