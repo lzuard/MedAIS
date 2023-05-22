63fa84e5ed18787f63f44ddd5aa71cac57e7268f
Izuard <sasha.20133@gmail.com>
2023-05-22 17:11:23 +0300

    Add Allergen entity

---
+++ App/Models/Entities/Allergen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace MedApp.Models.Entities
+{
+    class Allergen
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the Allergen
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// EF Navigation property
+        /// </summary>
+        public ICollection<Allergy> Allergies { get; set; }
+    }
+}
