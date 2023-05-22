7590a4540122846f40cfdf3c7cec7037987c5ae9
Izuard <sasha.20133@gmail.com>
2023-05-22 17:14:27 +0300

    Add Allergy entity

---
+++ App/Models/Entities/Allergy.cs
@@ -0,0 +1,25 @@
+﻿namespace MedApp.Models.Entities
+{
+    class Allergy
+    {
+        /// <summary>
+        /// Medical card ID
+        /// </summary>
+        public int MedCardId { get; set; }
+
+        /// <summary>
+        /// Allergen ID
+        /// </summary>
+        public int AllergenId { get;set; }
+
+        /// <summary>
+        /// EF Navigation property
+        /// </summary>
+        public MedCard MedCard { get; set; }
+
+        /// <summary>
+        /// ED Navigation property
+        /// </summary>
+        public Allergen Allergen { get; set; } 
+    }
+}
