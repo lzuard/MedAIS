bdadfdd379c8be4c9e5dc3da088f89d869efdaad
Izuard <sasha.20133@gmail.com>
2023-05-23 19:52:12 +0300

    Add base repository realization

---
+++ MedData/DbRepository.cs
@@ -0,0 +1,81 @@
+﻿using MedData.Data;
+using MedData.Entities.Base;
+using MedData.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedData
+{
+    public class DbRepository<T> : IRepository<T> where T : Entity, new()
+    {
+        private readonly ApplicationContext _context;
+        private readonly DbSet<T> _set;
+
+        public bool AutoSaveChanges = false;
+
+        public virtual IQueryable<T> Items => _set;
+
+
+        public DbRepository(ApplicationContext context) 
+        {
+            _context = context;
+            _set = context.Set<T>();
+        }
+
+
+        public T Get(int id) => Items.SingleOrDefault(i => i.Id == id);
+
+        public async Task<T> GetAsync(int id, CancellationToken cancel = default) => await Items
+            .SingleOrDefaultAsync(i => i.Id == id, cancel)
+            .ConfigureAwait(false);
+
+        public T Add(T entity)
+        {
+            if(entity is null) throw new ArgumentNullException(nameof(entity));
+            _context.Entry(entity).State = EntityState.Added;
+            if (AutoSaveChanges)
+                _context.SaveChanges();
+            return entity;
+        }
+
+        public async Task<T> AddAsync(T entity, CancellationToken cancel = default)
+        {
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+            _context.Entry(entity).State = EntityState.Added;
+            if (AutoSaveChanges)
+                await _context.SaveChangesAsync(cancel).ConfigureAwait(false);
+            return entity;
+        }
+
+        public void Update(T entity)
+        {
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+            _context.Entry(entity).State = EntityState.Modified;
+            if (AutoSaveChanges)
+                _context.SaveChanges();
+        }
+
+        public async Task UpdateAsync(T entity, CancellationToken cancel = default)
+        {
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+            _context.Entry(entity).State = EntityState.Modified;
+            if (AutoSaveChanges)
+                await _context.SaveChangesAsync(cancel).ConfigureAwait(false);
+        }
+
+        public void Delete(int id)
+        {
+            _context.Remove(new T { Id = id });
+
+            if(AutoSaveChanges)
+                _context.SaveChanges();
+        }
+
+        public async Task DeleteAsync(int id, CancellationToken cancel = default)
+        {
+            _context.Remove(new T { Id = id });
+
+            if (AutoSaveChanges)
+                await _context.SaveChangesAsync(cancel).ConfigureAwait(false);
+        }
+    }
+}
---
+++ MedData/Interfaces/IRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace MedData.Interfaces
+{
+    public interface IRepository<T> where T : class, IEntity, new()
+    {
+        /// <summary>
+        /// Contains all repository data
+        /// </summary>
+        IQueryable<T> Items { get; }
+
+        /// <summary>
+        /// Return entity by id
+        /// </summary>
+        T Get(int id);
+
+        /// <summary>
+        /// Async return entity by id
+        /// </summary>
+        Task<T> GetAsync(int id, CancellationToken cancel = default);
+
+        /// <summary>
+        /// Add entity to repository and returns it
+        /// </summary>
+        T Add(T entity);
+
+        /// <summary>
+        /// Async add entity to repository and returns it
+        /// </summary>
+        Task<T> AddAsync(T entity, CancellationToken cancel = default);
+        
+        /// <summary>
+        /// Updates the entity
+        /// </summary>
+        void Update(T entity);
+
+        /// <summary>
+        /// Async updates the entity
+        /// </summary>
+        Task UpdateAsync(T entity, CancellationToken cancel = default);
+
+        /// <summary>
+        /// Deletes entity by id
+        /// </summary>
+        void Delete(int id);
+
+        /// <summary>
+        /// Async deletes entity by id
+        /// </summary>
+        Task DeleteAsync(int id, CancellationToken cancel = default);
+    }
+}
