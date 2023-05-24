ea0aff02b0cea65bc4b6bc9c9951bef15829064e
Izuard <sasha.20133@gmail.com>
2023-05-24 14:20:25 +0300

    Add pages that can be changed inside the window

---
+++ App/ViewModels/DoctorsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MedApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedApp.ViewModels
+{
+    internal class DoctorsViewModel : ViewModelBase
+    {
+    }
+}
--- App/ViewModels/MainWindowViewModel.cs
+++ App/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MedApp.ViewModels.Base;
+﻿using MathCore.WPF.Commands;
+using MedApp.ViewModels.Base;
+using System.Windows.Input;
 
 namespace MedApp.ViewModels
 {
@@ -15,7 +17,56 @@ namespace MedApp.ViewModels
             set => Set(ref _title, value);
         }
         #endregion
+        #region Curent viewmodel
+        private ViewModelBase _currentViewModel;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => Set(ref _currentViewModel, value);
+        }
 
         #endregion
+
+        #endregion
+
+        #region Commands
+
+        #region Next view command
+        private ICommand _nextViewCommand;
+
+        public ICommand NextViewCommand => _nextViewCommand
+            ??= new LambdaCommand(OnNextViewCommandExecuted, CanNextViewCommandExecute);
+
+
+        private bool CanNextViewCommandExecute() => true;
+
+        private void OnNextViewCommandExecuted()
+        {
+            CurrentViewModel = new DoctorsViewModel();
+        }
+        #endregion
+        #region Previous view command
+        private ICommand _previousViewCommand;
+
+        public ICommand PreviousViewCommand => _previousViewCommand
+            ??= new LambdaCommand(OnPreviousViewCommandExecuted, CanPreviousViewCommandExecute);
+
+
+        private bool CanPreviousViewCommandExecute() => true;
+
+        private void OnPreviousViewCommandExecuted()
+        {
+            CurrentViewModel = new AuthViewModel();
+        }
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+
+        }
+
     }
 }
