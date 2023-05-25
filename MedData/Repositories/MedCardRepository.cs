19e7587f2921b0b4b51ea66e5202013abccb4dfb
Izuard <sasha.20133@gmail.com>
2023-05-26 01:46:21 +0300

    Add Medcard repository

---
+++ MedData/Repositories/MedCardRepository.cs
@@ -0,0 +1,15 @@
+﻿using MedData.Data;
+using MedData.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedData.Repositories
+{
+    internal class MedCardRepository : DbRepository<MedCard>
+    {
+        public override IQueryable<MedCard> Items => base.Items
+            .Include(i => i.Address)
+            .Include(i => i.PatientInChambers);
+
+        public MedCardRepository(ApplicationContext context) : base(context) { }
+    }
+}
