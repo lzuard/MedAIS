e93130f979eecfe66ad4b4daf0ade1ff693ddb52
Izuard <sasha.20133@gmail.com>
2023-05-26 01:46:39 +0300

    Add PatientInChambers repository

---
+++ MedData/Repositories/PatientInChambersRepository.cs
@@ -0,0 +1,17 @@
+﻿using MedData.Data;
+using MedData.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedData.Repositories
+{
+    internal class PatientInChambersRepository : DbRepository<PatientInChamber>
+    {
+        public override IQueryable<PatientInChamber> Items => base.Items
+            .Include(i => i.Chamber);
+
+        public PatientInChambersRepository(ApplicationContext context) : base(context)
+        {
+        }
+
+    }
+}
