7a5428c60c941ec103a0aaeb34be80655c71210e
Izuard <sasha.20133@gmail.com>
2023-05-28 17:41:26 +0300

    Add checkup window

---
+++ App/ViewModels/CheckupWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MedApp.ViewModels.Base;
+using MedData.Entities;
+
+namespace MedApp.ViewModels
+{
+    internal class CheckupWindowViewModel : ViewModelBase
+    {
+        #region CurrentCheckup
+
+        private Checkup? _currentCheckup;
+
+        public Checkup? CurrentCheckup
+        {
+            get => _currentCheckup;
+            set => Set(ref _currentCheckup, value);
+        }
+
+        #endregion CurrentCheckup
+
+
+
+        public void OpenExistingCheckup(Checkup checkup)
+        {
+            CurrentCheckup = checkup;
+        }
+
+        public void OpenNewCheckup(int hospitalizationId)
+        {
+            CurrentCheckup = new Checkup();
+        }
+    }
+}
