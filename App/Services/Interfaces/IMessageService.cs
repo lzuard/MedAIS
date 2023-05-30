60d1858b373a6ef6efdef79eee6cca289fbe1802
Izuard <sasha.20133@gmail.com>
2023-05-30 19:34:16 +0300

    Add service for showing message box

---
+++ App/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MedApp.Services.Interfaces
+{
+    interface IMessageService
+    {
+        public void ShowError(string message, string header);
+
+        public MessageBoxResult ShowQuestion(string message, string header,
+            MessageBoxResult defaultAnswer = MessageBoxResult.No);
+    }
+}
---
+++ App/Services/MessageService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MathCore.PE.Headers;
+using MedApp.Services.Interfaces;
+
+namespace MedApp.Services
+{
+    public class MessageService : IMessageService
+
+    {
+        public void ShowError(string message, string header)
+        {
+            MessageBox.Show(message, header, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public MessageBoxResult ShowQuestion(string message, string header, MessageBoxResult defaultAnswer = MessageBoxResult.No)
+        {
+            return MessageBox.Show(message, header, MessageBoxButton.YesNo, MessageBoxImage.Question, defaultAnswer);
+        }
+    }
+}
