fc36acc3a8772c17e9446ff9c67692112331bb9d
Izuard <sasha.20133@gmail.com>
2023-05-30 19:35:45 +0300

    Update service registrator

--- App/Services/ServicesRegistrator.cs
+++ App/Services/ServicesRegistrator.cs
@@ -7,15 +7,16 @@ namespace MedApp.Services
     static class ServicesRegistrator
     {
         public static IServiceCollection AddServices(this IServiceCollection services) => services
-        .AddTransient<IAuthService, AuthService>()
-        .AddTransient<IPatientsService, PatientService>()
-        .AddTransient<IWindowService, WindowService>()
-        .AddTransient<IEntitiesCollectionProvider<Department>,  EntityCollectionProvider<Department>>()
-        .AddTransient<IEntitiesCollectionProvider<Position>,  EntityCollectionProvider<Position>>()
-        .AddTransient<IEntitiesCollectionProvider<User>,  EntityCollectionProvider<User>>()
-        .AddTransient<IEntitiesCollectionProvider<Chamber>,  EntityCollectionProvider<Chamber>>()
-        .AddTransient<IEntitiesCollectionProvider<Cabinet>,  EntityCollectionProvider<Cabinet>>()
-        .AddTransient<IEntitiesCollectionProvider<Mkb>,  EntityCollectionProvider<Mkb>>()
+            .AddSingleton<IMessageService, MessageService>()
+            .AddTransient<IAuthService, AuthService>()
+            .AddTransient<IPatientsService, PatientService>()
+            .AddTransient<IWindowService, WindowService>()
+            .AddTransient<IEntitiesCollectionProvider<Department>,  EntityCollectionProvider<Department>>()
+            .AddTransient<IEntitiesCollectionProvider<Position>,  EntityCollectionProvider<Position>>()
+            .AddTransient<IEntitiesCollectionProvider<User>,  EntityCollectionProvider<User>>()
+            .AddTransient<IEntitiesCollectionProvider<Chamber>,  EntityCollectionProvider<Chamber>>()
+            .AddTransient<IEntitiesCollectionProvider<Cabinet>,  EntityCollectionProvider<Cabinet>>()
+            .AddTransient<IEntitiesCollectionProvider<Mkb>,  EntityCollectionProvider<Mkb>>()
         ;
     }
 }
