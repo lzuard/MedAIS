4ed2ad4bdcc163d15b33c80404dda96701c62a87
Izuard <sasha.20133@gmail.com>
2023-06-03 18:26:15 +0300

    Add csv to mkb mapper

---
+++ App/Services/Instruments/CsvToMkbMapper.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper.Configuration;
+using MedData.Entities;
+
+namespace MedApp.Services.Instruments
+{
+    public sealed class CsvToMkbMapper : ClassMap<Mkb>
+    {
+        public CsvToMkbMapper()
+        {
+            Map(mkb => mkb.Code).Index(2);
+            Map(mkb => mkb.Description).Index(3);
+        }
+    }
+}
