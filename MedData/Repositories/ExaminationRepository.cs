89b5db370d45ad61825e1f25b20b902fa866070b
Izuard <sasha.20133@gmail.com>
2023-06-03 22:01:29 +0300

    Add Examination repository

---
+++ MedData/Repositories/ExaminationRepository.cs
@@ -0,0 +1,24 @@
+﻿using MedData.Data;
+using MedData.Entities;
+using MedData.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedData.Repositories
+{
+    internal class ExaminationRepository : DbRepository<Examination>
+    {
+        public override IQueryable<Examination> Items => base.Items
+            .Include(i=>i.Hospitalization)
+            .Include(i=> i.Cabinet)
+            .Include(i=> i.ExaminationType)
+            .Include(i=> i.User)
+        ;
+
+        public ExaminationRepository(ApplicationContext context) : base(context) { }
+    }
+}
