ae91e910e285ad635c01468c9ca98ba7ac0d8fea
Izuard <sasha.20133@gmail.com>
2023-06-04 22:04:15 +0300

    Add service for working with diagnoses

---
+++ App/Services/DiagnosisService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MedApp.Services.Interfaces;
+using MedData.Entities;
+using MedData.Interfaces;
+
+namespace MedApp.Services
+{
+    public class DiagnosisService : IDiagnosisService
+    {
+        private readonly IRepository<Mkb> _mkbRepo;
+        private readonly IRepository<Diagnosis> _diagnosisRepo;
+
+        /// <summary>
+        /// Get list of all mkb entities
+        /// </summary>
+        public IEnumerable<Mkb> GetMkbList() => _mkbRepo.Items.ToList();
+
+        /// <summary>
+        /// Get list of all patient diagnosis
+        /// </summary>
+        public IEnumerable<Diagnosis> GetDiagnosisList(int hospitalizationId) =>
+            _diagnosisRepo.Items.Where(d => d.HospitalizationId == hospitalizationId);
+
+        /// <summary>
+        /// Save changes in diagnosis
+        /// </summary>
+        public bool SaveChanges(IEnumerable<Diagnosis> diagnosisList, Hospitalization hospitalization)
+        {
+            try
+            {
+                var curDate = DateTime.Now.ToUniversalTime();
+                foreach (var diagnosis in diagnosisList)
+                {
+                    if (diagnosis.Id == 0)
+                    {
+                        diagnosis.Hospitalization = hospitalization;
+                        diagnosis.Date = curDate;
+                        _diagnosisRepo.Add(diagnosis);
+                    }
+                    else
+                        _diagnosisRepo.Update(diagnosis);
+                }
+
+                _diagnosisRepo.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public DiagnosisService(IRepository<Mkb> mkbRepo, IRepository<Diagnosis> diagnosisRepo)
+        {
+            _mkbRepo = mkbRepo;
+            _diagnosisRepo = diagnosisRepo;
+        }
+    }
+}
---
+++ App/Services/Interfaces/IDiagnosisService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedData.Entities;
+
+namespace MedApp.Services.Interfaces
+{
+    public interface IDiagnosisService
+    {
+        public IEnumerable<Mkb> GetMkbList();
+
+        public IEnumerable<Diagnosis> GetDiagnosisList(int hospitalizationId);
+
+        public bool SaveChanges(IEnumerable<Diagnosis> diagnosisList, Hospitalization hospitalization);
+    }
+}
